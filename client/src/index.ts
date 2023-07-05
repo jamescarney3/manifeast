20f3b5c145891cf34fb1a42e953e293bf4aee547
James Carney <jmcarneyiii@gmail.com>
2023-07-05 17:05:36 -0400

    add react and test harness skeleton to client

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,1 +1,12 @@
-console.log('hello world');
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+
+const domNode = document.getElementById('mount');
+const root = createRoot(domNode);
+
+root.render(React.createElement(() => ('hello react app')));
+
+if (module.hot) {
+  module.hot.accept();
+}
---
+++ client/test/index.test.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+
+
+describe('placeholder', () => {
+  it('runs a test', () => {
+    expect(true).to.equal(true);
+  });
+});
