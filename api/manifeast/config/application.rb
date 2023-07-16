226298424f9b7349c7f77b8257efa1d77178ba24
James Carney <jmcarneyiii@gmail.com>
2023-07-16 15:13:29 -0400

    install and enable olive branch

--- api/manifeast/config/application.rb
+++ api/manifeast/config/application.rb
@@ -24,19 +24,6 @@ module App
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    # not thrilled that I can't seem to handle this in the "session_store"
-    # initializer because ...it doesn't work, or seem to do much of anything
-    # really - need to look into further because this almost defintiely
-    # belongs in an initializer
-    # see: https://github.com/waiting-for-dev/devise-jwt/issues/235
-    # update: this is why - https://guides.rubyonrails.org/api_app.html#using-session-middlewares
-    # config.session_store :cookie_store, key: '_manifeast_session'
-    # config.action_dispatch.cookies_same_site_protection = :none
-    # config.middleware.use ActionDispatch::Cookies
-    # config.middleware.use ActionDispatch::Session::CookieStore
-    # config.middleware.use config.session_store, config.session_options
-
-    # for https in development, god willing
-    # config.force_ssl=true
+    config.middleware.use OliveBranch::Middleware, inflection: 'camel'
   end
 end
